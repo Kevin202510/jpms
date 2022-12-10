73456ad8f64dc7589a62aed53619371e85aa6103
unknown <silent.kevin.f.caluag20@gmail.com>
2022-12-10 00:51:24 -0700

    zxc

--- index.php
+++ index.php
@@ -158,7 +158,7 @@ if(isset($_POST['register'])) {
             <div class="col-lg-12">
                 <div class="section-tittle text-center">
                     
-                    <h2>Browse Top Categories </h2>
+                    <h2>Jobs</h2>
                 </div>
             </div>
         </div>
@@ -437,7 +437,7 @@ if(isset($_POST['register'])) {
 
                                 <div class="modal-footer" style="background-color:#13e9a5;">
                                     
-                            <button style="border-radius:20px; text-align:center;" type="submit" class="btn " id="subreg" name="register">REGISTERED</button>
+                            <button style="border-radius:20px;" type="submit" class="btn " id="subreg" name="register">REGISTERED</button>
                                      
                                     </div>
                          </form>
@@ -520,6 +520,150 @@ if(isset($_POST['register'])) {
 
 
 
+
+<div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="viewModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="viewModalLabel">View</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+             
+                <div class="modal-body">
+
+                <!-- job post company Start -->
+        <div class="job-post-company pt-120 pb-120">
+            <div class="container">
+                <div class="row justify-content-between">
+                    <!-- Left Content -->
+                    <div class="col-xl-7 col-lg-8">
+                        <!-- job single -->
+
+
+                        <div class="single-job-items mb-50">
+                 
+                            <div class="job-items">
+                                <div class="company-img company-img-details">
+                                    <img src="assets/img/icon/job-list1.png" alt="">
+                                </div>
+                                <div class="job-tittle">
+                                    
+                                        <h4 id="job_company_namess"></h4>
+                                   
+                                    <ul>
+                                        <li id="jobs_namess"></li>
+                                        <li id="jobs_addressss"></li>
+                                        <li id="job_expected_salaryss"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                           
+                        </div>
+                          <!-- job single End -->
+                       
+                        <div class="job-post-details">
+                            <div class="post-details1 mb-50">
+                                <!-- Small Section Tittle -->
+                                <div class="small-section-tittle">
+                                    <h4>Job Description</h4>
+                                </div>
+                                <p id="jobs_descriptionss"></p>
+                            </div>
+                            <div class="post-details2  mb-50">
+                                 <!-- Small Section Tittle -->
+                                <div class="small-section-tittle">
+                                    <h4>Required Knowledge, Skills, and Abilities</h4>
+                                </div>
+                               <ul>
+                                   <li id="jobs_r_skillsss"></li>
+                                  
+                               </ul>
+                            </div>
+                            <div class="post-details2  mb-50">
+                                 <!-- Small Section Tittle -->
+                                <div class="small-section-tittle">
+                                    <h4>Education + Experience</h4>
+                                </div>
+                               <ul>
+                                   <li id="ea_name"></li>
+
+                                   <li id="jobs_r_experiencess"></li>
+                                   
+                               </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- Right Content -->
+                    <div class="col-xl-4 col-lg-4">
+                        <div class="post-details3  mb-50">
+                            <!-- Small Section Tittle -->
+                           <div class="small-section-tittle">
+                               <h4>Job Overview</h4>
+                           </div>
+                          <ul>
+                              <li>Posted date : <span id="created_at"></span></li>
+                              <li>Location : <span id="jobs_addresssss"></span></li>
+                              <li>Vacancy : <span id="jobs_vacancy_countss"></span></li>
+                              <li>Job nature : <span id="jobs_preferred_timess"></span></li>
+                              <li>Salary :  <span id="job_expected_salarysss"></span></li>
+                              <li>Application date : <span>12 Sep 2020</span></li>
+                          </ul>
+                         <!-- <div class="apply-btn2">
+                            <a href="#" class="btn">Apply Now</a>
+                         </div> -->
+                        
+                       </div>
+                        <div class="post-details4  mb-50">
+                            <!-- Small Section Tittle -->
+                           <div class="small-section-tittle">
+                               <h4>Company Information</h4>
+                           </div>
+                             
+                            <ul>
+                                <li>Name: <span id="full_namess"></span></li>
+                                <li>Contact: <span id="user_contactss"></span></li>
+                                <li>Email: <span id="user_emailss"></span></li>
+                            </ul>
+                       </div>
+                    </div>
+                </div>
+            </div>
+       
+        </div>
+        
+                  </div>
+              
+                </div>
+            </div>
+         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php include('applicantsviews/footer.php'); ?>
 <?php include('applicantsviews/script.php'); ?>
 
@@ -553,6 +697,47 @@ if(isset($_POST['register'])) {
             alert("PASSWORD DIDN'T MATCH");
         }
     });
+
+    $("body").on('click','#view',function(e){
+
+var USER_IDss = $(e.currentTarget).data('id');
+// alert(USER_IDss);
+$.post("update_jobs.php",{USER_IDsss: USER_IDss},function(data,status){
+    var emp = JSON.parse(data);
+    console.log(emp[0]);
+    $("#jobs_idss").text(emp[0].jobs_id);
+    let newdate = new Date(emp[0].created_at);
+    var day = newdate.getDate();
+    var month = newdate.getMonth() + 1;
+    var year = newdate.getFullYear();
+
+    $("#created_at").text(month+" / "+day+" / "+year);
+    $("#jobs_user_idss").text(emp[0].jobs_user_id);
+    $("#job_company_namess").text(emp[0].job_company_name);
+    $("#jobs_namess").text(emp[0].jobs_name);
+    $("#jobs_addressss").text(emp[0].jobs_address);
+    $("#job_expected_salaryss").text(emp[0].job_expected_salary);
+    $("#jobs_descriptionss").text(emp[0].jobs_description);
+    $("#jobs_r_skillsss").text(emp[0].jobs_r_skills);
+    $("#ea_name").text(emp[0].ea_name);
+    $("#jobs_r_experiencess").text(emp[0].jobs_r_experience);
+    $("#jobs_addresssss").text(emp[0].jobs_address);
+    $("#jobs_vacancy_countss").text(emp[0].jobs_vacancy_count);
+    $("#jobs_preferred_timess").text(emp[0].jobs_preferred_time);
+    $("#job_expected_salarysss").text(emp[0].job_expected_salary);
+    $("#full_namess").text(emp[0].user_fname + " " + emp[0].user_lname);
+    $("#user_contactss").text(emp[0].user_contact);
+    $("#user_emailss").text(emp[0].user_email);
+    // alert("user_contactss");
+});  
+
+
+$("#viewModal").modal("show");
+
+});
+
+
+
   })
   
 
